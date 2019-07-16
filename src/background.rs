b7abd512f05023112466daeca30d02238c4389d5
Matt Thomas <me@matthewathomas.com>
2019-07-15 20:43:58 -0400

    Implement all formatting traits

--- src/background.rs
+++ src/background.rs
@@ -19,16 +19,23 @@ impl<T> Background for &T {
     }
 }
 
-impl<T> fmt::Display for WithBackground<T>
-where T: fmt::Display {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "\x1B[48;2;{};{};{}m{}\x1B[0m", self.rgb.r, self.rgb.g, self.rgb.b, self.t)
-    }
+macro_rules! impl_me {
+    ($bound:path, $format_arg:expr) => {
+        impl<T> $bound for WithBackground<T>
+        where T: $bound {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                write!(f, concat!("\x1B[48;2;{};{};{}m", $format_arg, "\x1B[0m"), self.rgb.r, self.rgb.g, self.rgb.b, self.t)
+            }
+        }
+    };
 }
 
-impl<T> fmt::Debug for WithBackground<T>
-where T: fmt::Debug {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "\x1B[48;2;{};{};{}m{:?}\x1B[0m", self.rgb.r, self.rgb.g, self.rgb.b, self.t)
-    }
-}
+impl_me!(fmt::Binary, "{:b}");
+impl_me!(fmt::Debug, "{:?}");
+impl_me!(fmt::Display, "{}");
+impl_me!(fmt::LowerExp, "{:e}");
+impl_me!(fmt::LowerHex, "{:x}");
+impl_me!(fmt::Octal, "{:o}");
+impl_me!(fmt::Pointer, "{:p}");
+impl_me!(fmt::UpperExp, "{:E}");
+impl_me!(fmt::UpperHex, "{:X}");
--- src/foreground.rs
+++ src/foreground.rs
@@ -19,16 +19,23 @@ impl<T> Foreground for &T {
     }
 }
 
-impl<T> fmt::Display for WithForeground<T>
-where T: fmt::Display {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "\x1B[38;2;{};{};{}m{}\x1B[0m", self.rgb.r, self.rgb.g, self.rgb.b, self.t)
-    }
+macro_rules! impl_me {
+    ($bound:path, $format_arg:expr) => {
+        impl<T> $bound for WithForeground<T>
+        where T: $bound {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                write!(f, concat!("\x1B[38;2;{};{};{}m", $format_arg, "\x1B[0m"), self.rgb.r, self.rgb.g, self.rgb.b, self.t)
+            }
+        }
+    };
 }
 
-impl<T> fmt::Debug for WithForeground<T>
-where T: fmt::Debug {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "\x1B[38;2;{};{};{}m{:?}\x1B[0m", self.rgb.r, self.rgb.g, self.rgb.b, self.t)
-    }
-}
+impl_me!(fmt::Binary, "{:b}");
+impl_me!(fmt::Debug, "{:?}");
+impl_me!(fmt::Display, "{}");
+impl_me!(fmt::LowerExp, "{:e}");
+impl_me!(fmt::LowerHex, "{:x}");
+impl_me!(fmt::Octal, "{:o}");
+impl_me!(fmt::Pointer, "{:p}");
+impl_me!(fmt::UpperExp, "{:E}");
+impl_me!(fmt::UpperHex, "{:X}");
