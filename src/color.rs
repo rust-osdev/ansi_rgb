ed81de8b270ee1b84476664944c7f7cc38f74768
Han Mertens <hanmertens@outlook.com>
2020-08-17 21:28:06 +0200

    Make epilogue a provided method
    It should be the same for all colors, but can be overwritten this way.

--- src/color.rs
+++ src/color.rs
@@ -3,5 +3,7 @@ use core::fmt;
 
 pub trait Color {
     fn prelude(&self, f: &mut fmt::Formatter, canvas: Canvas) -> fmt::Result;
-    fn epilogue(&self, f: &mut fmt::Formatter, canvas: Canvas) -> fmt::Result;
+    fn epilogue(&self, f: &mut core::fmt::Formatter, _canvas: crate::Canvas) -> core::fmt::Result {
+        f.write_str("\x1B[0m")
+    }
 }
